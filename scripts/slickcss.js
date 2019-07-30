d3a68a684275fd9cf235ed856e3e53468e4c6ec2
eveOch <52388784+eveOch@users.noreply.github.com>
2019-07-30 16:31:16 +0300

    trial version

---
+++ scripts/slickcss.js
@@ -0,0 +1,19 @@
+
+$('.slick-center').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    if (currentSlide == "0")
+    {
+        $('header').css('backgroundColor', '');
+        $('header li a').css('color', 'black');
+        $('header span a').css('color', 'black');
+        $('.slider-text a').css('color', 'black');
+        $(".order img").attr("src","/assets/cart.png");
+    }
+    else
+    {
+        $('header').css('backgroundColor', 'rgb(0,0,0, 0.5');
+        $('header li a').css('color', 'white');
+        $('header span a').css('color', 'white');
+        $('.slider-text a').css('color', 'white');
+        $(".order img").attr("src","/assets/cart2.png");
+    }
+});
